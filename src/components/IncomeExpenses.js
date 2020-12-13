78efa22bda7cde8426d5c920eb2e01efecda9ff2
Himanish <himanish.kaushal@gmail.com>
2020-12-13 02:42:25 -0500

    Add state (transactions)

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+    let income = 0;
+    let expense = 0;
+    transactions.map(t => {
+        if (t.amount < 0)
+            expense += t.amount;
+        else
+            income += t.amount;
+    });
+        
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p id="money-plus" className="money plus">+$0.00</p>
+                <p id="money-plus" className="money plus">{income.toFixed(2)}</p>
                 </div>
             <div>
                 <h4>Expense</h4>
-                <p id="money-minus" className="money minus">-$0.00</p>
+                <p id="money-minus" className="money minus">{Math.abs(expense).toFixed(2)}</p>
             </div>
         </div>
     )
---
+++ src/components/Transaction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Transaction = ({ transaction }) => {
+    const { text, amount } = transaction;
+    const sign = amount < 0 ? '-' : '+';
+
+    return (
+        <div>
+            <li className={amount < 0 ? "minus" : "plus"}>
+                {text} <span>{sign}${Math.abs(amount)}</span><button className="delete-btn">x</button>
+            </li>
+        </div>
+    )
+}
